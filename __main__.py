27822a6b59a9e445a18099582de12903bed5a672
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-14 10:38:14 +0100

    feat(auto_learner.py): Adds auto train for grasping locations

--- __main__.py
+++ __main__.py
@@ -8,5 +8,12 @@ if __name__ == "__main__":
     vec_neems_path = args[1]
     training_data_result_path = args[2]
 
+    print 'Transforming NEEMs into vectors ...'
     auto_learner.transform_raw_neems_to_vec_neems(raw_neems_path, vec_neems_path)
+    print 'DONE Transforming NEEMs into vectors'
+    print 'Extracting grasping data from vectors...'
     auto_learner.generate_grasping_learning_data(vec_neems_path, training_data_result_path)
+    print 'DONE Extracting grasping data from vectors'
+    print 'Training grasping models ...'
+    auto_learner.generate_grasping_models(training_data_result_path)
+    print 'DONE Training grasping models'
--- auto_learner/auto_learner.py
+++ auto_learner/auto_learner.py
@@ -4,11 +4,12 @@ from os.path import join
 from narrative2vec import Narrative
 from grasping_learning_data_generator.orientation import generate_learning_data_from_neems as generate_orientation_data
 from grasping_learning_data_generator.position import generate_learning_data_from_neems as generate_position_data
+import grasping_position_inference.training.model_generator
 
 
 def transform_raw_neems_to_vec_neems(path, result_dir_path):
     for neemName in listdir(path):
-            neem_path = join(path, neemName)
+            neem_path = join(path, neemName,'{}.owl'.format(neemName))
 
             narrative = Narrative(neem_path)
             narrative.transform_to_csv_file(result_dir_path)
@@ -17,3 +18,8 @@ def transform_raw_neems_to_vec_neems(path, result_dir_path):
 def generate_grasping_learning_data(path, result_dir_path):
     generate_position_data(path, result_dir_path)
     generate_orientation_data(path, result_dir_path)
+
+
+def generate_grasping_models(training_data_path):
+    grasping_position_inference.training.model_generator.generate_models(training_data_path)
+
